a021ae0f2f030869c30fe01a735b6aaf9c4b247e
Artem <gizatulin511@gmail.com>
2025-03-09 01:39:32 +0300

    feat: Add theme-based image gallery with dynamic navigation - Implement AJAX image switching - Add theme selection modal - Configure responsive image container styling

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem "sprockets-rails"
 gem "bootstrap"
 gem "font-awesome-sass"
 gem "bcrypt"
+gem "jquery-rails"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def image_orientation(file_path)
+    image = MiniMagick::Image.new(Rails.root.join('app/assets/images/pictures', file_path))
+    image.width >= image.height ? 'landscape' : 'portrait'
+  rescue
+    'landscape'
+  end
 end
